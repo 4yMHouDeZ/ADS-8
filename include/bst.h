c519d90bbe4d3fb97e4dfad56b42980c1a8cafdf
4yMHouDeZ <sergoart1234@gmail.com>
2026-06-09 23:52:54 +0300

    att 1

--- include/bst.h
+++ include/bst.h
@@ -2,4 +2,91 @@
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
 
+#include <string>
+
+struct Item {
+  std::string key;
+  unsigned int value;
+};
+
+template <typename T>
+class BST {
+ private:
+  struct Node {
+    T key;
+    unsigned int value;
+    Node* left = nullptr;
+    Node* right = nullptr;
+  };
+  Node* root;
+  int depth(Node* current, int count) const {
+    if (current == nullptr)
+      return count;
+    count++;
+    return std::max(depth(current->left, count), depth(current->right, count));
+  }
+  void remove(Node* current) {
+    if (current->left != nullptr)
+      remove(current->left);
+    if (current->right != nullptr)
+      remove(current->right);
+    delete current;
+  }
+  void reqToVec(std::vector<Item>& arr, Node* current) {
+    if (current == nullptr) return;
+    arr.push_back({current->key, current->value});
+    reqToVec(arr, current->left);
+    reqToVec(arr, current->right);
+  }
+ public:
+  BST() : root(nullptr) {}
+  ~BST() {
+    remove(root);
+  }
+  void insert(const T& key) {
+    if (root == nullptr) {
+      root = new Node {key, 1};
+      return;
+    }
+    Node* parent = nullptr;
+    Node* current = root;
+    while (current != nullptr) {
+      if (key == current->key) {
+        current->value++;
+        return;
+      } else if (key < current->key) {
+        parent = current;
+        current = current->left;
+      } else {
+        parent = current;
+        current = current->right;
+      }
+    }
+    if (key < parent->key)
+      parent->left = new Node {key, 1};
+    else
+      parent->right = new Node {key, 1};
+  }
+
+  int depth() const {
+    return depth(root, -1);
+  }
+
+  int search(const T& key) const {
+    Node* current = root;
+    while (current != nullptr) {
+      if (key == current->key)
+        return current->value;
+      else if (key < current->key)
+        current = current->left;
+      else
+        current = current->right;
+    }
+    return 0;
+  }
+  void toVector(std::vector<Item>& arr) {
+    reqToVec(arr, root);
+  }
+};
+
 #endif  // INCLUDE_BST_H_
--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,36 @@
 #include  "bst.h"
 
 void makeTree(BST<std::string>& tree, const char* filename) {
-  // поместите сюда свой код
+    std::ifstream file(filename);
+
+    if (!file) {
+        std::cout << "File error!" << std::endl;
+        return;
+    }
+    std::string word;
+    while(!file.eof())
+    {
+      char ch=file.get();
+      if (isalpha(ch)) {
+        word += std::tolower(ch);
+      } else {
+        if (!word.empty()) {
+          tree.insert(word);
+        word.clear();
+        }
+      }
+    }
+    if (!word.empty()) tree.insert(word);
+    file.close();
+}
+
+void printFreq(BST<std::string>& tree) {
+  std::vector<Item> arr;
+  tree.toVector(arr);
+  std::sort(arr.begin(), arr.end(), [](Item a, Item b) {
+    return a.value > b.value || a.key < b.key && a.value == b.value;
+  });
+  for (int i = 0; i < arr.size(); i++) {
+    std::cout << arr[i].key << " - " << arr[i].value << " times" << std::endl;
+  }
 }
